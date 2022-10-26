6732ec36fffbd2d94197aa5d603d692bd02d49d8
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-10-26 22:00:34 +0300

    инициализация проекта

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "airbnb-base",
+    "plugin:vue/essential",
+    "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["vue", "prettier"],
+  rules: {
+    "import/prefer-default-export": "off",
+    "vue/multi-word-component-names": [
+      "error",
+      {
+        ignores: ["default", "error", "Index", "Error"],
+      },
+    ],
+    "no-unused-expressions": ["error", { allowTernary: true, allowShortCircuit: true }],
+
+    "vue/no-deprecated-slot-scope-attribute": "off",
+    "vue/no-deprecated-v-on-native-modifier": "off",
+    "vue/no-deprecated-slot-attribute": "off",
+    "import/no-extraneous-dependencies": "off",
+    "no-unused-vars": "off",
+    "no-return-assign": "off",
+    "no-restricted-syntax": "off",
+  },
+};
--- README.md
+++ README.md
@@ -1,24 +1,35 @@
 # block-canvas
 
+на проекте используется версия node 18.10.0
+
+## Правила для EsLint
+
+В [этом файле](EsLint.md) есть вся необходимая информация по правилам, которые используются в проекте.
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
