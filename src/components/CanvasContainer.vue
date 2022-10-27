d1d9f0fbe9982a22ca05650735b1f43bc8824e8b
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-10-27 13:15:00 +0300

    контейнер, где будет блок с canvas

---
+++ src/components/CanvasContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">здесь будет Canvas</div>
+</template>
+
+<script>
+export default {
+  name: "CanvasContainer",
+};
+</script>
+
+<style scoped>
+.container {
+  width: 1440px;
+  background-color: aqua;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>Проверка</div>
+  <CanvasContainer class="test" />
 </template>
 
 <script>
+import CanvasContainer from "@/components/CanvasContainer.vue";
 export default {
   name: "HomePage",
+  components: {
+    CanvasContainer,
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.test {
+  margin: 30px auto;
+}
+</style>
