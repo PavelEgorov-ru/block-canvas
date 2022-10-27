1624751d61eccb95143574309dcf5e843c24851d
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-10-27 20:17:05 +0300

    удален ненужный параметр опций canvas

--- src/utils/canvasUtils.js
+++ src/utils/canvasUtils.js
@@ -3,9 +3,7 @@ import { fabric } from "fabric";
 let canvas = null;
 
 export const canvasInit = (id) => {
-  canvas = new fabric.Canvas(id, {
-    isDrawingMode: true,
-  });
+  canvas = new fabric.Canvas(id, {});
 };
 
 export const canvasUnmount = () => {
