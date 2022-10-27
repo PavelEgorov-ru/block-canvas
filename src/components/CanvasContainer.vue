7b45c345696a037784763ef9a7017381281c1385
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-10-27 13:51:17 +0300

    инициализация canvas

--- src/components/CanvasContainer.vue
+++ src/components/CanvasContainer.vue
@@ -1,16 +1,36 @@
 <template>
-  <div class="container">здесь будет Canvas</div>
+  <div class="container">
+    <h1>Блок над Canvas</h1>
+    <canvas id="canvas" class="canvas" width="1000" height="600"></canvas>
+  </div>
 </template>
 
 <script>
+import { canvasInit, canvasUnmount } from "@/utils/canvasUtils";
+
 export default {
   name: "CanvasContainer",
+  mounted() {
+    canvasInit("canvas");
+  },
+  beforeDestroy() {
+    canvasUnmount();
+  },
 };
 </script>
 
 <style scoped>
 .container {
   width: 1440px;
-  background-color: aqua;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.canvas {
+  border: 1px solid red;
+  width: 1000px;
+  height: 600px;
 }
 </style>
