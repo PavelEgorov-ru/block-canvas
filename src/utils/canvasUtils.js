ce5b565958e88a108e028fb897cd6008e5357186
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-10-30 22:16:48 +0300

    функция для подготовки линии

--- src/utils/canvasUtils.js
+++ src/utils/canvasUtils.js
@@ -28,21 +28,17 @@ export const canvasInit = (id) => {
 
 export function addNewElement(canvas, connection) {
   let isCircleDown = false;
-
   const rect = newRect();
-
   const circleArr = [];
-
   const group = newGroup([rect, ...circleArr]);
-
-  console.log(group);
-
+  group.on("moving", () => console.log("навели на элемент"));
   for (const key in connection) {
     if (key === "top" && connection[key]) {
       const circleTop = newCircle(circlePosition.top);
       group.add(circleTop);
-      circleTop.on("mousedown", function () {
+      circleTop.on("mousedown", function (event) {
         listenerCircleMouseDown(canvas, circleTop, group, isCircleDown);
+        newLine(event, canvas);
       });
       circleTop.on("mouseup", function () {
         listenerCircleMouseUp(canvas, circleTop, group, isCircleDown);
@@ -51,8 +47,9 @@ export function addNewElement(canvas, connection) {
     if (key === "right" && connection[key]) {
       const circleLeft = newCircle(circlePosition.left);
       group.add(circleLeft);
-      circleLeft.on("mousedown", function () {
+      circleLeft.on("mousedown", function (event) {
         listenerCircleMouseDown(canvas, circleLeft, group, isCircleDown);
+        newLine(event, canvas);
       });
       circleLeft.on("mouseup", function () {
         listenerCircleMouseUp(canvas, circleLeft, group, isCircleDown);
@@ -61,8 +58,9 @@ export function addNewElement(canvas, connection) {
     if (key === "left" && connection[key]) {
       const circleRight = newCircle(circlePosition.right);
       group.add(circleRight);
-      circleRight.on("mousedown", function () {
+      circleRight.on("mousedown", function (event) {
         listenerCircleMouseDown(canvas, circleRight, group, isCircleDown);
+        newLine(event, canvas);
       });
       circleRight.on("mouseup", function () {
         listenerCircleMouseUp(canvas, circleRight, group, isCircleDown);
@@ -71,8 +69,9 @@ export function addNewElement(canvas, connection) {
     if (key === "bottom" && connection[key]) {
       const circleBottom = newCircle(circlePosition.bottom);
       group.add(circleBottom);
-      circleBottom.on("mousedown", function () {
+      circleBottom.on("mousedown", function (event) {
         listenerCircleMouseDown(canvas, circleBottom, group, isCircleDown);
+        newLine(event, canvas);
       });
       circleBottom.on("mouseup", function () {
         listenerCircleMouseUp(canvas, circleBottom, group, isCircleDown);
@@ -100,7 +99,7 @@ function listenerCircleMouseDown(canvas, circleObj, groupObj, isCircleDown) {
 }
 
 function listenerCircleMouseUp(canvas, circleObj, groupObj, isCircleDown) {
-  console.log("отпустил мышку");
+  canvas.off("mouse:move", deleteEvents());
   isCircleDown = false;
   const colorCircle = isCircleDown ? "red" : "balck";
   circleObj.set({
@@ -110,9 +109,20 @@ function listenerCircleMouseUp(canvas, circleObj, groupObj, isCircleDown) {
     lockMovementX: false,
     lockMovementY: false,
   });
+
   canvas.renderAll();
 }
 
+function newLine(event, canvas) {
+  console.log("событие при нажатии мышки", event);
+  canvas.on("mouse:move", (options) => {
+    console.log(options);
+  });
+}
+
+function deleteEvents() {
+  console.log("удалился обработчик событий");
+}
 const newRect = () => {
   return new fabric.Rect({
     width: 50,
